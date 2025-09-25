ee8b0fe03f1b175c6bd048489699302b77234513
Crysre <tamosrk@gmail.com>
2025-09-26 01:17:44 +0530

    added footer

--- bookmarkwebsite/src/components/Footer.jsx
+++ bookmarkwebsite/src/components/Footer.jsx
@@ -3,12 +3,12 @@ import twitter from "../../images/icon-twitter.svg"
 import facebook from "../../images/icon-facebook.svg"
 
 export function Footer(){
-    return <footer className=" mx-auto flex justify-between space-y-16   bg-[#252b46] w-full h-15" >
+    return <footer className=" py-16 text-white px-20 mx-auto flex justify-between space-y-16  bg-[#252b46] w-full h-15" >
         <div className=" space-x-14  flex  " >
-            <img className="h-10"   src={bookmarkLogo} alt="" />
-                <a>FEATURES</a>
-                <a>DOWNLOAD</a>
-                <a>FAQ</a>
+            <img className="mb-1 h-6"   src={bookmarkLogo} alt="" />
+                <a className="hover:text-[#fa5757] tracking-wider text-gray-500" >FEATURES</a>
+                <a className="hover:text-[#fa5757] tracking-wider text-gray-500" >DOWNLOAD</a>
+                <a className="hover:text-[#fa5757] tracking-wider text-gray-500" >FAQ</a>
         </div>
         <div className=" space-x-10 flex" >
             <img className=" h-6"  src={facebook} alt="" />
